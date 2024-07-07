ef442d88f604c5fdc3d117804a31466d3b5afabe
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-07 19:43:46 +0100

    style: change the colour palette

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,23 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = "#0a7ea4";
-const tintColorDark = "#fff";
-
 export const Colors = {
   light: {
     text: "#11181C",
-    background: "#fff",
-    tint: tintColorLight,
+    background: "#ECEDEE",
+    highlight: "#e74043",
+    tint: "#ebaa39",
     icon: "#687076",
     tabIconDefault: "#687076",
-    tabIconSelected: tintColorLight,
+    tabIconSelected: "#ebaa39",
   },
   dark: {
     text: "#ECEDEE",
     background: "#22222d",
-    tint: tintColorDark,
+    highlight: "#e74043",
+    tint: "#ebaa39",
     icon: "#9BA1A6",
     tabIconDefault: "#9BA1A6",
-    tabIconSelected: tintColorDark,
+    tabIconSelected: "#ebaa39",
   },
 };
