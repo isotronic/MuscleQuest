0f300360644f0e061a5774fb09284cc4c233e20d
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-13 11:12:53 +0100

    refactor: change card to button and display only first name

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { ThemedView } from "@/components/ThemedView";
 import { ScrollView } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { View } from "react-native";
-import { Button, Card, Title } from "react-native-paper";
+import { Button, Card } from "react-native-paper";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import WeekDays from "@/components/WeekDays";
 import { useContext } from "react";
@@ -21,6 +21,8 @@ const dummyWorkouts = [
 export default function HomeScreen() {
   const user = useContext(AuthContext);
   const insets = useSafeAreaInsets();
+  const userName = ", " + user?.displayName?.split(" ")[0] || "";
+
   return (
     <ThemedView>
       <ScrollView
@@ -38,42 +40,49 @@ export default function HomeScreen() {
           </ThemedText>
         </View>
         <View style={styles.welcomeContainer}>
-          <ThemedText type="title">Welcome {user?.displayName}</ThemedText>
+          <ThemedText type="subtitle">Welcome{userName}</ThemedText>
           <ThemedText type="default">
             Your journey to Swoletown begins today!
           </ThemedText>
         </View>
         <View style={styles.cardContainer}>
-          <Card
+          <Button
+            mode="contained"
             onPress={() => console.log("Today's workout pressed")}
-            style={styles.card}
+            style={styles.cardButton}
+            contentStyle={styles.cardContent}
           >
-            <Card.Content>
-              <Title style={styles.todayCardTitle}>
+            <View>
+              <ThemedText style={styles.todayCardTitle}>
                 Today's Workout: Chest and Triceps
-              </Title>
-            </Card.Content>
-            <Card.Cover
-              source={{
-                uri: "https://images.unsplash.com/photo-1652363722833-509b3aac287b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-              }}
-            />
-          </Card>
+              </ThemedText>
+              <View style={styles.imageContainer}>
+                <Card.Cover
+                  source={{
+                    uri: "https://images.unsplash.com/photo-1652363722833-509b3aac287b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  }}
+                  style={styles.cardImage}
+                />
+              </View>
+            </View>
+          </Button>
         </View>
         <View style={styles.otherWorkoutsContainer}>
           <ThemedText type="default" style={styles.sectionTitle}>
             Upcoming Workouts
           </ThemedText>
           {dummyWorkouts.map((workout) => (
-            <Card
+            <Button
               key={workout.id}
+              mode="contained"
               onPress={() => console.log(`${workout.title} pressed`)}
-              style={styles.smallCard}
+              style={styles.smallCardButton}
+              contentStyle={styles.smallCardContent}
             >
-              <Card.Content>
-                <Title style={styles.cardTitle}>{workout.title}</Title>
-              </Card.Content>
-            </Card>
+              <ThemedText type="subtitle" style={styles.cardTitle}>
+                {workout.title}
+              </ThemedText>
+            </Button>
           ))}
         </View>
         <View style={styles.buttonContainer}>
@@ -121,9 +130,24 @@ const styles = StyleSheet.create({
   cardContainer: {
     padding: 20,
   },
-  card: {
+  cardButton: {
     borderRadius: 15,
     backgroundColor: Colors.dark.cardBackground,
+    padding: 0,
+    width: "100%",
+  },
+  cardContent: {
+    alignItems: "flex-start",
+    width: "100%",
+  },
+  imageContainer: {
+    width: "100%",
+    borderRadius: 15,
+    overflow: "hidden",
+  },
+  cardImage: {
+    marginTop: 10,
+    width: "100%",
   },
   todayCardTitle: {
     color: Colors.dark.text,
@@ -135,10 +159,14 @@ const styles = StyleSheet.create({
   sectionTitle: {
     marginBottom: 10,
   },
-  smallCard: {
-    marginBottom: 10,
+  smallCardButton: {
+    marginBottom: 15,
     borderRadius: 15,
     backgroundColor: Colors.dark.cardBackground,
+  },
+  smallCardContent: {
+    height: 60,
+    justifyContent: "center",
   },
   cardTitle: {
     color: Colors.dark.text,
