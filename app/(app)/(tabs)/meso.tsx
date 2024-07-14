dc931c7e30b2fb12cc9a05fb3bd7db89a354786e
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-14 10:01:50 +0100

    feat: disable / delete meso and stats screens

--- app/(app)/(tabs)/meso.tsx
+++
@@ -1,4 +0,0 @@
-import { ThemedView } from "@/components/ThemedView";
-export default function MesoCycleScreen() {
-  return <ThemedView style={{ flex: 1, backgroundColor: "red" }}></ThemedView>;
-}
