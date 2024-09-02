1bd4aea690c6f5038a728b1bf30826abbd0c9c19
Joseph Bouqdib <joseph@bouqdib.com>
2024-09-02 19:37:59 +0100

    Fetch and display user created plans

--- app/(app)/(tabs)/plans.tsx
+++ app/(app)/(tabs)/plans.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import TrainingPlanCard from "@/components/TrainingPlanCard";
@@ -5,10 +6,54 @@ import { Colors } from "@/constants/Colors";
 import { router } from "expo-router";
 import { FlatList, ScrollView, StyleSheet } from "react-native";
 import { FAB } from "react-native-paper";
+import { fetchAllRecords } from "@/utils/database";
+import { Workout } from "@/store/store";
+import { useFocusEffect } from "@react-navigation/native";
+
+interface TrainingPlan {
+  plan_data: object;
+  name: string;
+  image_url: string;
+}
 
 export default function PlansScreen() {
+  const [plans, setPlans] = useState<Workout[]>([]);
+
+  const fetchPlans = async () => {
+    try {
+      const plans = (await fetchAllRecords(
+        "userData.db",
+        "user_plans",
+      )) as Workout[];
+      setPlans(plans);
+    } catch (error) {
+      console.error("Error fetching plans", error);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchPlans();
+    }, []),
+  );
+
   const handleCreatePlan = () => {
     router.push("/(app)/(create-plan)/create");
+  };
+
+  // const handleEditPlan = () => {
+  //   router.push("/(app)/(create-plan)/edit");
+  // };
+
+  const renderTrainingPlanCard = ({ item }: { item: TrainingPlan }) => {
+    // const planData = JSON.stringify(item.plan_data);
+    return (
+      <TrainingPlanCard
+        title={item.name}
+        imageUrl={item.image_url}
+        onPress={() => {}}
+      />
+    );
   };
   return (
     <ThemedView style={styles.container}>
@@ -19,8 +64,8 @@ export default function PlansScreen() {
           contentContainerStyle={styles.scrollViewContainer}
           snapToInterval={320}
           snapToAlignment={"start"}
-          data={[1, 2, 3, 4, 5, 6]}
-          renderItem={() => <TrainingPlanCard />}
+          data={plans}
+          renderItem={renderTrainingPlanCard}
           keyExtractor={(item: number, index: number) => index.toString()}
         />
         <ThemedText style={styles.sectionTitle}>Build muscle</ThemedText>
@@ -30,7 +75,7 @@ export default function PlansScreen() {
           snapToInterval={320}
           snapToAlignment={"start"}
           data={[1, 2, 3, 4, 5, 6]}
-          renderItem={() => <TrainingPlanCard />}
+          renderItem={() => <TrainingPlanCard onPress={() => {}} />}
           keyExtractor={(item: number, index: number) => index.toString()}
         />
         <ThemedText style={styles.sectionTitle}>Gain strength</ThemedText>
@@ -40,7 +85,7 @@ export default function PlansScreen() {
           snapToInterval={320}
           snapToAlignment={"start"}
           data={[1, 2, 3, 4, 5, 6]}
-          renderItem={() => <TrainingPlanCard />}
+          renderItem={() => <TrainingPlanCard onPress={() => {}} />}
           keyExtractor={(item: number, index: number) => index.toString()}
         />
 
