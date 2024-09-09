ec840e7f784f2cdb72f6550c439bea1e51e2fda6
Joseph Bouqdib <joseph@bouqdib.com>
2024-09-09 14:29:38 +0100

    Refactor to reduce complexity

--- hooks/useActivePlanQuery.ts
+++ hooks/useActivePlanQuery.ts
@@ -1,42 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
-import { Plan } from "./usePlans";
+import { Plan } from "./useAllPlansQuery";
 import { fetchActivePlan } from "@/utils/database";
 
-const fetchAndParse = async () => {
+const fetchAndParse = async (): Promise<Plan | undefined> => {
   try {
-    const activePlan = (await fetchActivePlan()) as Plan | null;
+    const activePlan = (await fetchActivePlan()) as Plan | undefined;
 
     if (
-      activePlan &&
-      typeof activePlan === "object" &&
-      Object.keys(activePlan).length > 0
+      !activePlan ||
+      typeof activePlan !== "object" ||
+      Object.keys(activePlan).length === 0
     ) {
-      let parsedPlan = activePlan;
-
-      if (typeof activePlan.plan_data === "string") {
-        try {
-          parsedPlan.plan_data = JSON.parse(activePlan.plan_data);
-        } catch (jsonError) {
-          console.error("Error parsing plan_data:", jsonError);
-          return;
-        }
-      }
-
-      return parsedPlan;
-    } else if (activePlan === undefined) {
-      return;
-    } else {
-      return;
+      return undefined;
     }
+
+    return {
+      ...activePlan,
+      plan_data:
+        typeof activePlan.plan_data === "string"
+          ? JSON.parse(activePlan.plan_data)
+          : activePlan.plan_data,
+    };
   } catch (error) {
-    console.error("Error fetching plan", error);
+    console.error("Error fetching or parsing plan", error);
+    return undefined;
   }
 };
 
 export const useActivePlanQuery = () => {
-  return useQuery<Plan>({
+  return useQuery<Plan | undefined>({
     queryKey: ["activePlan"],
-    queryFn: () => fetchAndParse() as Promise<Plan>,
+    queryFn: fetchAndParse,
     staleTime: Infinity,
   });
 };
