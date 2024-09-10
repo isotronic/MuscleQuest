0277ba9c42817a3ac6c46307be720e8cd43dcd58
Joseph Bouqdib <joseph@bouqdib.com>
2024-09-10 09:02:08 +0100

    Add workout session to navigation stack

---
+++ app/(app)/(workout)/_layout.tsx
@@ -0,0 +1,20 @@
+import { Colors } from "@/constants/Colors";
+import { Stack } from "expo-router";
+
+export default function WorkoutLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Colors.dark.background,
+        },
+        headerTintColor: Colors.dark.text,
+      }}
+    >
+      <Stack.Screen
+        name="workout-session"
+        options={{ title: "Workout Session" }}
+      />
+    </Stack>
+  );
+}
