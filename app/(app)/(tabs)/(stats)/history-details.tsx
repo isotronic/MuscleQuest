6fad9bbd3d44b9e884cfc849d4c798342556debe
Joseph Bouqdib <joseph@bouqdib.com>
2024-09-20 12:49:07 +0100

    Improve exercise layout

--- app/(app)/(tabs)/(stats)/history-details.tsx
+++ app/(app)/(tabs)/(stats)/history-details.tsx
@@ -1,9 +1,8 @@
-// screens/WorkoutDetail.tsx
-import React, { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { View, StyleSheet, ScrollView, Image } from "react-native";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
-import { ActivityIndicator } from "react-native-paper";
+import { ActivityIndicator, Card } from "react-native-paper";
 import { Colors } from "@/constants/Colors";
 import { useLocalSearchParams } from "expo-router";
 import { CompletedWorkout } from "@/hooks/useCompletedWorkoutsQuery";
@@ -13,6 +12,7 @@ import {
 } from "@/utils/database";
 import { byteArrayToBase64 } from "@/utils/utility";
 import { parseISO, format } from "date-fns";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function HistoryDetailsScreen() {
   const { workoutId } = useLocalSearchParams();
@@ -88,11 +88,38 @@ export default function HistoryDetailsScreen() {
             {workout.workout_name}
           </ThemedText>
           <ThemedText style={styles.workoutDate}>
-            Completed on {formattedDate}
+            Completed on: {formattedDate}
           </ThemedText>
-          <ThemedText style={styles.totalVolume}>
-            Total Volume: {totalVolume} kg
-          </ThemedText>
+        </View>
+        <View style={styles.summaryRow}>
+          <View style={styles.summaryItem}>
+            <MaterialCommunityIcons
+              name="clock"
+              size={24}
+              color={Colors.dark.icon}
+            />
+            <ThemedText style={styles.summaryText}>
+              {workout.duration} mins
+            </ThemedText>
+          </View>
+          <View style={styles.summaryItem}>
+            <MaterialCommunityIcons
+              name="numeric"
+              size={24}
+              color={Colors.dark.icon}
+            />
+            <ThemedText style={styles.summaryText}>
+              {workout.total_sets_completed} sets
+            </ThemedText>
+          </View>
+          <View style={styles.summaryItem}>
+            <MaterialCommunityIcons
+              name="scale"
+              size={24}
+              color={Colors.dark.icon}
+            />
+            <ThemedText style={styles.summaryText}>{totalVolume} kg</ThemedText>
+          </View>
         </View>
 
         {/* Exercise List */}
@@ -106,7 +133,7 @@ export default function HistoryDetailsScreen() {
           }
 
           return (
-            <View key={exercise.exercise_id} style={styles.exerciseSection}>
+            <Card key={exercise.exercise_id} style={styles.exerciseCard}>
               <View style={styles.exerciseHeader}>
                 {imageUri ? (
                   <Image
@@ -129,11 +156,11 @@ export default function HistoryDetailsScreen() {
                     Set {set.set_number}
                   </ThemedText>
                   <ThemedText style={styles.setText}>
-                    Reps: {set.reps} | Weight: {set.weight} kg
+                    {set.weight} kg | {set.reps} Reps
                   </ThemedText>
                 </View>
               ))}
-            </View>
+            </Card>
           );
         })}
       </ScrollView>
@@ -147,33 +174,51 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   topSection: {
-    marginBottom: 32,
+    marginBottom: 24,
     alignItems: "center",
   },
   workoutName: {
-    fontSize: 24,
+    fontSize: 28,
+    lineHeight: 28,
     fontWeight: "bold",
   },
   workoutDate: {
     fontSize: 16,
-    marginTop: 8,
+    marginTop: 4,
   },
-  totalVolume: {
-    fontSize: 18,
-    marginTop: 8,
-    fontWeight: "bold",
-  },
-  exerciseSection: {
+  summaryRow: {
+    flexDirection: "row",
+    justifyContent: "space-around",
     marginBottom: 16,
+  },
+  summaryItem: {
+    alignItems: "center",
+  },
+  summaryText: {
+    fontSize: 16,
+    marginTop: 4,
+  },
+  exerciseCard: {
+    marginBottom: 16,
+    borderRadius: 8,
+    paddingBottom: 8,
+    elevation: 2, // For Android
+    shadowColor: "#000", // For iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    backgroundColor: Colors.dark.cardBackground,
   },
   exerciseHeader: {
     flexDirection: "row",
     alignItems: "center",
+    padding: 16,
   },
   exerciseImage: {
-    width: 50,
-    height: 50,
-    marginRight: 12,
+    width: 60,
+    height: 60,
+    borderRadius: 8,
+    marginRight: 16,
   },
   exerciseName: {
     fontSize: 20,
