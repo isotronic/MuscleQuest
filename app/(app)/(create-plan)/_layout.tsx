7501fdf067e8dcd90710a34fea548fa1d18f6a08
Joseph Bouqdib <joseph@bouqdib.com>
2024-10-14 09:57:57 +0100

    Move exercise details out of create plan stack for reuse

--- app/(app)/(create-plan)/_layout.tsx
+++ app/(app)/(create-plan)/_layout.tsx
@@ -17,10 +17,6 @@ export default function CreatePlanLayout() {
         options={{ title: "Images by Unsplash" }}
       />
       <Stack.Screen name="exercises" options={{ title: "Exercises" }} />
-      <Stack.Screen
-        name="exercise-details"
-        options={{ title: "Exercise Details" }}
-      />
       <Stack.Screen name="sets-overview" options={{ title: "Sets Overview" }} />
     </Stack>
   );
