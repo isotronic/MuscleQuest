c8b5f3e029089c6acf03d45367d3d30bc6371967
Joseph Bouqdib <joseph@bouqdib.com>
2024-11-08 16:56:35 +0000

    Remove sentry metro config

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,6 @@
 /* eslint-env node */
-// This replaces `const { getDefaultConfig } = require('expo/metro-config');`
-const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+const { getDefaultConfig } = require("expo/metro-config");
 
-// This replaces `const config = getDefaultConfig(__dirname);`
-const config = getSentryExpoConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
 module.exports = config;
