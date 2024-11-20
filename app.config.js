42b2454a7b9b404c49dd338846371855f400fc04
Joseph Bouqdib <joseph@bouqdib.com>
2024-11-20 07:30:17 +0000

    feat: add new body weight exercises (#51)
    * Copy over new appData2
    
    * Copy new exercises from appData2
    
    * Update version number

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: IS_DEV ? "Muscle Quest (Dev)" : "Muscle Quest",
     slug: "musclequest",
-    version: "0.8.8",
+    version: "0.8.9",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "myapp",
