8db5f2c957e5e6110e24237aae3d0e2b3a8935ed
Joseph Bouqdib <joseph@bouqdib.com>
2025-02-03 15:52:21 +0000

    Switch from unsplash api to list of images

--- app/(app)/(create-plan)/image-search.tsx
+++ app/(app)/(create-plan)/image-search.tsx
@@ -1,176 +1,45 @@
-import React, { useEffect, useState, useCallback } from "react";
-import {
-  Alert,
-  FlatList,
-  TouchableOpacity,
-  StyleSheet,
-  View,
-  Linking,
-  TextInput,
-} from "react-native";
+import React from "react";
+import { FlatList, TouchableOpacity, StyleSheet, View } from "react-native";
 import { Image } from "expo-image";
-import { Button } from "react-native-paper";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { createApi } from "unsplash-js";
 import { useNavigation } from "expo-router";
-import { Basic } from "unsplash-js/dist/methods/photos/types";
 import { ThemedView } from "@/components/ThemedView";
 import { useWorkoutStore } from "@/store/workoutStore";
-import { ThemedText } from "@/components/ThemedText";
-import { Colors } from "@/constants/Colors";
-import Bugsnag from "@bugsnag/expo";
-
-const unsplash = createApi({
-  accessKey: process.env.EXPO_PUBLIC_UNSPLASH_ACCESS_KEY || "",
-});
+import { PlanImages } from "@/constants/PlanImages";
 
 export default function ImageSearchScreen() {
-  const [query, setQuery] = useState("");
-  const [searchQuery, setSearchQuery] = useState("bodybuilding gym");
-  const [photos, setPhotos] = useState<React.SetStateAction<Basic[]>>([]);
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
   const { setPlanImageUrl } = useWorkoutStore();
   const navigation = useNavigation();
 
-  const handleSearch = useCallback(async () => {
-    setPage(1); // Reset page when doing a new search
-    setLoading(true);
-    try {
-      const response = await unsplash.search.getPhotos({
-        query: searchQuery,
-        page: 1,
-        perPage: 20,
-        orientation: "landscape",
-      });
-      setPhotos(response.response?.results || []);
-    } catch (error: any) {
-      console.error("Error fetching images from Unsplash:", error);
-      Bugsnag.notify(error);
-      Alert.alert(
-        "Error",
-        "Failed to fetch images from Unsplash. Please try again.",
-        [{ text: "OK" }],
-      );
-    } finally {
-      setLoading(false);
-    }
-  }, [searchQuery]); // Depend on searchQuery
-
-  useEffect(() => {
-    handleSearch();
-  }, [handleSearch]);
-
-  const handleLoadMore = async () => {
-    setLoading(true);
-    const nextPage = page + 1;
-    try {
-      const response = await unsplash.search.getPhotos({
-        query: searchQuery,
-        page: nextPage,
-        perPage: 20,
-      });
-      const newPhotos = response.response?.results || [];
-      setPhotos((prevPhotos: Basic[]) => [...prevPhotos, ...newPhotos]);
-      setPage(nextPage);
-    } catch (error: any) {
-      console.error("Error loading more images:", error);
-      Bugsnag.notify(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   const handleImageSelect = (url: string) => {
-    setPlanImageUrl(url);
+    setPlanImageUrl(`${url}?utm_source=MuscleQuest&utm_medium=referral`);
     navigation.goBack();
   };
 
-  const openPhotographerProfile = (url: string) => {
-    Linking.openURL(`${url}?utm_source=MuscleQuest&utm_medium=referral`);
-  };
-
-  const handleSearchSubmit = () => {
-    setSearchQuery(query); // Use the text input value for the search query
-  };
+  const photoArray = Object.entries(PlanImages).map(([key, url]) => ({
+    id: key,
+    uri: url,
+  }));
 
   return (
     <ThemedView>
-      <View style={styles.searchContainer}>
-        <TextInput
-          placeholder="Search for images"
-          value={query}
-          onChangeText={setQuery}
-          style={styles.searchInput}
-          selectTextOnFocus={true}
-          onSubmitEditing={handleSearchSubmit}
-          placeholderTextColor={Colors.dark.subText}
-        />
-        <MaterialCommunityIcons
-          name="magnify"
-          size={24}
-          color={Colors.dark.text}
-          style={styles.iconButton}
-          onPress={handleSearchSubmit}
-        />
-      </View>
       <FlatList
-        data={photos}
-        keyExtractor={(item: Basic) => item.id}
+        data={photoArray}
+        keyExtractor={(item: { id: string; uri: string }) => item.id}
         numColumns={2}
-        renderItem={({ item }: { item: Basic }) => (
+        renderItem={({ item }: { item: { id: string; uri: string } }) => (
           <View style={styles.imageContainer}>
-            <TouchableOpacity
-              onPress={() => handleImageSelect(item.urls.regular)}
-            >
-              <Image source={{ uri: item.urls.small }} style={styles.image} />
+            <TouchableOpacity onPress={() => handleImageSelect(item.uri)}>
+              <Image source={{ uri: item.uri }} style={styles.image} />
             </TouchableOpacity>
-            <View style={styles.photographerBanner}>
-              <TouchableOpacity
-                onPress={() => openPhotographerProfile(item.user.links.html)}
-              >
-                <ThemedText style={styles.photographerText}>
-                  {item.user.name}
-                </ThemedText>
-              </TouchableOpacity>
-            </View>
           </View>
         )}
         contentContainerStyle={styles.grid}
-        ListFooterComponent={
-          photos.length > 0 && (
-            <View style={styles.loadMoreContainer}>
-              <Button onPress={handleLoadMore} disabled={loading}>
-                {loading ? "Loading..." : "Load More"}
-              </Button>
-            </View>
-          )
-        }
       />
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  searchContainer: {
-    position: "relative",
-    marginBottom: 16,
-    padding: 16,
-  },
-  searchInput: {
-    padding: 10,
-    borderColor: Colors.dark.text,
-    borderWidth: 1,
-    borderRadius: 8,
-    color: Colors.dark.text,
-    fontSize: 18,
-    paddingRight: 40,
-  },
-  iconButton: {
-    position: "absolute",
-    right: 24,
-    top: "58%",
-  },
   imageContainer: {
     position: "relative",
     margin: 4,
@@ -180,25 +49,8 @@ const styles = StyleSheet.create({
     width: 175,
     height: 150,
   },
-  photographerBanner: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    overflow: "hidden",
-  },
-  photographerText: {
-    color: "#FFFFFF",
-    fontSize: 12,
-    textAlign: "center",
-  },
   grid: {
     justifyContent: "center",
-    alignItems: "center",
-  },
-  loadMoreContainer: {
-    paddingVertical: 20,
     alignItems: "center",
   },
 });
