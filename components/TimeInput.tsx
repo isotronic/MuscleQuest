95ae849322f83de5ae7d5ed15f89b264efa04776
Joseph Bouqdib <joseph@bouqdib.com>
2025-03-27 18:29:43 +0000

    refactor: simplify TimeInput

--- components/TimeInput.tsx
+++ components/TimeInput.tsx
@@ -1,5 +1,5 @@
 // TimeInput.tsx
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { TextInput, TextInputProps } from "react-native";
 import { formatTimeInput } from "@/utils/utility";
 
@@ -17,21 +17,14 @@ export const TimeInput = ({
   style,
   props,
 }: TimeInputProps) => {
-  const [localTime, setLocalTime] = useState(value);
-
-  useEffect(() => {
-    setLocalTime(value);
-  }, [value]);
-
   const handleChange = (text: string) => {
     const sanitized = text.replace(/[^0-9]/g, "");
-    setLocalTime(sanitized);
     onChange(sanitized);
   };
 
   return (
     <TextInput
-      value={formatTimeInput(localTime)}
+      value={formatTimeInput(value)}
       onChangeText={handleChange}
       keyboardType="numeric"
       selectTextOnFocus
