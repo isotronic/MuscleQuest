baa4b4f24079be0b7d3d659cccb6854634d07a89
Joseph Bouqdib <joseph@bouqdib.com>
2025-03-30 12:19:59 +0100

    feat: add what's new message for "to failure" sets

--- constants/WhatsNew.ts
+++ constants/WhatsNew.ts
@@ -17,4 +17,12 @@ export const WHATS_NEW_ENTRIES: WhatsNewEntry[] = [
 You can now add notes to exercises, workouts and plans. Use this to jot down technique tips, machine settings, reminders, or anything else that helps you crush your session!
 `,
   },
+  {
+    version: 1600,
+    message: `
+🔥 New Feature: To Failure Sets!
+
+You can now mark a set to "To Failure" to indicate that you want to complete the set to failure. Especially useful for isolation machine, dumbbell, or bodyweight exercises, where you want to push yourself to the limit.
+`,
+  },
 ];
