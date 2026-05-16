3e97e4e4129511c788eaeb1b5a44757270136f51
Joseph Bouqdib <joseph@bouqdib.com>
2026-05-16 09:07:59 +0100

    feat: add new charts for volume and sparkline, and implement chart theme

---
+++ components/charts/chartTheme.ts
@@ -0,0 +1,17 @@
+import { Colors } from "@/constants/Colors";
+
+export const chartTheme = {
+  gridColor: "transparent",
+  axisColor: Colors.dark.subText,
+  labelColor: Colors.dark.subText,
+  labelSize: 10,
+  primaryColor: Colors.dark.tint,
+  prColor: Colors.dark.tint,
+  negativeColor: Colors.dark.highlight,
+  areaStartFill: "rgba(235,170,57,0.4)",
+  areaEndFill: "rgba(235,170,57,0)",
+  thickness: 2,
+  dataPointRadius: 4,
+  barBorderRadius: 4,
+  noOfSections: 3,
+};
